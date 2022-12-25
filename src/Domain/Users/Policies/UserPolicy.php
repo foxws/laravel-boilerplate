ee42fdec5a4d8879c90af5c54e77b7f2978bae8b
francoism90 <francoism90@users.noreply.github.com>
2022-12-25 16:34:21 +0100

    wip

--- src/Domain/Users/Policies/UserPolicy.php
+++ src/Domain/Users/Policies/UserPolicy.php
@@ -5,87 +5,46 @@ namespace Domain\Users\Policies;
 use Domain\Users\Models\User;
 use Domain\Users\Models\User as UserModel;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    public function viewAny(User $user)
+    public function viewAny(User $user): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, UserModel $model)
+    public function view(User $user, UserModel $model): Response|bool
     {
         dd('true');
 
         return true;
-        //
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function create(User $user): Response|bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, UserModel $model)
+    public function update(User $user, UserModel $model): Response|bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, UserModel $model)
+    public function delete(User $user, UserModel $model): Response|bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, UserModel $model)
+    public function restore(User $user, UserModel $model): Response|bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, UserModel $model)
+    public function forceDelete(User $user, UserModel $model): Response|bool
     {
-        //
+        return false;
     }
 }
