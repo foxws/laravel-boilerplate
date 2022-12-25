222457a82d4c57192e47d4c5479f4d39b29d801b
francoism90 <francoism90@users.noreply.github.com>
2022-12-25 14:12:04 +0100

    wip

--- src/Domain/Users/Collections/UserDataCollection.php
+++ src/Domain/Users/Collections/UserDataCollection.php
@@ -5,14 +5,11 @@ namespace Domain\Users\Collections;
 use Domain\Users\DataObjects\UserData;
 use Domain\Users\Models\User;
 use Illuminate\Database\Eloquent\Collection;
-use Spatie\LaravelData\DataCollection;
 
 class UserDataCollection extends Collection
 {
-    public function dataCollection(): DataCollection
+    public function dataCollection(): self
     {
-        return UserData::collection(
-            $this->map(fn (User $model) => UserData::fromModel($model))
-        );
+        return $this->map(fn (User $model) => UserData::fromModel($model));
     }
 }
