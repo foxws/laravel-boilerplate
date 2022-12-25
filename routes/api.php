87472df3351f1701ee4cb954a3311a7ff7e6520c
francoism90 <francoism90@users.noreply.github.com>
2022-12-25 20:45:10 +0100

    Remove api prefix

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('api.')->prefix('api/v1')->group(function () {
+Route::name('api.')->prefix('v1')->group(function () {
     Route::apiResources([
         'users' => UserController::class,
         // 'posts' => PostController::class,
--- routes/auth.php
+++ routes/auth.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AuthenticateController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('auth.')->prefix('api/v1')->group(function () {
+Route::name('auth.')->prefix('v1')->group(function () {
     $limiter = config('fortify.limiters.login');
 
     Route::post('/login', [AuthenticateController::class, 'store'])
