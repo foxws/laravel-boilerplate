ed1eb8fd04e9ab4f2690591b2b5138aab98be5b2
francoism90 <francoism90@users.noreply.github.com>
2023-01-05 10:08:40 +0100

    wip

--- src/Domain/Posts/Support/QueryBuilder/Filters/FilterList.php
+++ src/Domain/Posts/Support/QueryBuilder/Filters/FilterList.php
@@ -9,6 +9,11 @@ class FilterList implements Filter
 {
     public function __invoke(Builder $query, mixed $value, string $property): void
     {
+        $ids = [1,2,3];
+
+        $query->whereIn('id', $ids)
+            ->orderByRaw('FIELD (id, ' . implode(', ', $ids) . ') ASC');
+
         // $query->whereHas('permissions', function (Builder $query) use ($value) {
         //     $query->where('name', $value);
         // });
