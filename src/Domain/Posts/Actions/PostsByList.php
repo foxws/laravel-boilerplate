5160e0297a97c66a8ef631ff546bcbe1d633f197
francoism90 <francoism90@users.noreply.github.com>
2023-01-05 14:44:20 +0100

    wip

---
+++ src/Domain/Posts/Actions/PostsByList.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Domain\Posts\Actions;
+
+use Domain\Posts\Models\Post;
+use Illuminate\Support\Collection;
+
+class PostsByList
+{
+    public function execute(string $value): Collection
+    {
+        return match ($value) {
+            'feed' => $this->getUserFeed(),
+            // default => $this->getByList($value)
+        };
+    }
+
+    protected function getUserFeed(): Collection
+    {
+        return Post::select('id')
+            ->inRandomOrder()
+            ->take(500)
+            ->get();
+    }
+}
--- src/Domain/Posts/Support/QueryBuilder/Filters/ListFilter.php
+++ src/Domain/Posts/Support/QueryBuilder/Filters/ListFilter.php
@@ -2,7 +2,7 @@
 
 namespace Domain\Posts\Support\QueryBuilder\Filters;
 
-use Domain\Posts\Actions\BuildUserFeed;
+use Domain\Posts\Actions\PostsByList;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
 
@@ -10,7 +10,7 @@ class ListFilter implements Filter
 {
     public function __invoke(Builder $query, mixed $value, string $property): void
     {
-        $action = app(BuildUserFeed::class)->execute();
+        $action = app(PostsByList::class)->execute($value);
 
         $ids = $action->pluck('id')->toArray();
         $idsOrder = implode(', ', $ids);
